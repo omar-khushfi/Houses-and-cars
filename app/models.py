708085249df676c18f1140859bc66013594a6f4c
omar-khushfi <omar.khu.2004@gmail.com>
2024-12-17 23:31:27 +0300

    commit 3

--- app/models.py
+++ app/models.py
@@ -1,86 +1,230 @@
+import datetime
 from django.db import models
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 import os
 
 
-class Merchant(models.Model):
+class Seller(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     password=models.CharField(max_length=50)
     phone = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
-    country = models.CharField(max_length=30)
-
+    image = models.ImageField(upload_to="images/erchant_images/")
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
+class Feature(models.Model):
+	feature = models.CharField( max_length=50)
+	rank = models.PositiveSmallIntegerField(default=9)
+
+	class Meta:
+			ordering = ["feature"]
+
+	def __str__(self):
+			return self.feature
 
 
 ###################Car#################
-# class Car(models.Model):
-#     merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE, related_name="cars")
-#     make = models.CharField(max_length=50)
-#     car_class = models.CharField(max_length=50)
-#     model = models.CharField(max_length=50)
-#     mileage = models.CharField(max_length=20)
-#     color = models.CharField(max_length=30)
-#     inside_color = models.CharField(max_length=30)
-#     car_type = models.CharField(max_length=30)
-#     gear_type = models.CharField(max_length=20)
-#     wheel_drive = models.CharField(max_length=50)
-#     seat_type = models.CharField(max_length=30)
-#     year = models.PositiveIntegerField()
-#     cylinder = models.PositiveIntegerField()
-#     gps = models.BooleanField(default=False)
-#     cd = models.BooleanField(default=False)
-#     dvd = models.BooleanField(default=False)
-#     rear_camera = models.BooleanField(default=False)
-#     sensors = models.BooleanField(default=False)
-#     sun_roof = models.BooleanField(default=False)
-#     with_warranty = models.BooleanField(default=False)
-#     bluetooth = models.BooleanField(default=False)
-#     fuel_type = models.CharField(max_length=20)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     def __str__(self):
-#         return f"{self.make} {self.model} ({self.year})"
+
+class Car(models.Model):
+	regional_spec_choices = (
+		('g', 'GCC Specs'),
+		('a', 'American Specs'),
+		('c', 'Canadian Specs'),
+		('e', 'European Specs'),
+		('j', 'Japanese Specs'),
+		('k', 'Korean Specs'),
+		('n', 'Chinese Specs'),
+		('x', 'Other'),
+	)
+
+	fuel_type_choices = (
+		('p', 'Petrol'),
+		('d', 'Diesel'),
+		('e', 'Electric'),
+		('h', 'Hybrid'),
+	)
+	
+	warranty_choices = (
+		('y', 'Yes'),
+		('n', 'No'),
+		('x', 'Does not apply'),
+	)
+	
+	car_status_options = (
+		('a','Active'),
+		('d','Deactive'),
+		('s','Sold'),
+	)
+	transmission_type_choices = (
+		('a', 'Automatic'),
+		('m', 'Manual'),
+	)
+
+	steering_side_choices = (
+		('l', 'Left Hand'),
+		('r', 'Right Hand'),
+	)
+
+	doors_choices = (
+		('2', '2 doors'),
+		('3', '3 doors'),
+		('4', '4 doors'),
+		('5', '5 doors'),
+		('6', '6+ doors'),
+	)
+
+	seating_choices = (
+		('2', '2 Seater'),
+		('4', '4 Seater'),
+		('5', '5 Seater'),
+		('6', '6 Seater'),
+		('7', '7 Seater'),
+		('8', '8 Seater'),
+		('9', '9+ Seater'),
+	)
+
+	cylinders_choices = (
+		('3', '3'),
+		('4', '4'),
+		('5', '5'),
+		('6', '6'),
+		('8', '8'),
+		('0', '10'),
+		('2', '12'),
+		('u', 'Unknown'),
+	)
+
+	body_type_choices = (
+		('sv', 'SUV'),
+		('cp', 'Coupe'),
+		('se', 'Sedan'),
+		('xo', 'Crossover'),
+		('ht', 'Hard Top Convertible'),
+		('pt', 'Pick Up Truck'),
+		('hb', 'Hatchback'),
+		('st', 'Soft Top Convertible'),
+		('sc', 'Sports Car'),
+		('vn', 'Van'),
+		('wg', 'Wagon'),
+		('ut', 'Utility Truck'),
+		('oc', 'Other'),
+	)
+	
+	engine_capacity_choices = (
+		('a', '0 - 499 cc'),
+		('b', '500 - 999 cc'),
+		('c', '1000 - 1499 cc'),
+		('d', '1500 - 1999 cc'),
+		('e', '2000 - 2499 cc'),
+		('f', '2500 - 2999 cc'),
+		('g', '3000 - 3499 cc'),
+		('h', '3500 - 3999 cc'),
+		('i', '4000+ cc'),
+		('u', 'Unknown'),
+	)
+
+	horsepower_choices = (
+		('a', '0 - 99 HP'),
+		('b', '100 - 199 HP'),
+		('c', '200 - 299 HP'),
+		('d', '300 - 399 HP'),
+		('e', '400 - 499 HP'),
+		('f', '500 - 599 HP'),
+		('g', '600 - 699 HP'),
+		('h', '700 - 799 HP'),
+		('i', '800 - 899 HP'),
+		('j', '900+ HP'),
+		('u', 'Unknown'),
+	)
+
+	city_choices = (
+		('d', 'Damascus'),
+		('h', 'Homs'),
+		('a', 'Aleppo'),
+		('da', 'Daraa'),
+		('hm', 'Hama'),
+		('dr', 'Deir-ezzour'),
+		('i', 'Idlib'),
+		('t', 'Tartus'),
+		('l', 'Latakia'),
+		('r', 'Al-Ruqqa'),
+	)
+
+	year_choice = []
+	plus = 2 if datetime.now().month >= 8 else 1
+	for r in range(2000, (datetime.now().year + plus)):
+		year_choice.append((r,r))
+
+    
+	title = models.CharField( max_length=100)	
+	make = models.CharField( max_length=100)
+	model = models.CharField( max_length=100)
+	trim = models.CharField( max_length=100, blank=True )
+	year = models.PositiveSmallIntegerField(('year'), choices=year_choice)
+	kilometers = models.PositiveIntegerField(blank=True, null=True )
+	exterior_color = models.CharField( max_length=100, blank=True )
+	interior_color = models.CharField( max_length=100, blank=True )
+	regional_spec = models.CharField(max_length=1, choices=regional_spec_choices, blank=True, default= 'g' )
+	fuel_type = models.CharField(max_length=1, choices=fuel_type_choices, default= 'petrol')
+	transmission = models.CharField(max_length=1, choices=transmission_type_choices, default= 'automatic')
+	body_type = models.CharField(max_length=2, choices=body_type_choices, blank=True )
+	doors = models.CharField(max_length=1, choices=doors_choices, blank=True )
+	seating = models.CharField(max_length=1, choices=seating_choices, blank=True )
+	engine_capacity = models.CharField(max_length=1, choices=engine_capacity_choices, blank=True )
+	cylinders = models.CharField(max_length=1, choices=cylinders_choices, blank=True )
+	horsepower = models.CharField(max_length=1, choices=horsepower_choices, blank=True )
+	steering_side = models.CharField(max_length=1, choices=steering_side_choices, blank=True, default= 'l' )
+	warranty = models.CharField(max_length=1, choices=warranty_choices, blank=True)
+	selling_price = models.PositiveIntegerField(blank=True, null=True)
+	description = models.TextField(blank=True )
+	status = models.CharField(max_length=1, choices=car_status_options, default= 'active')
+	is_featured = models.BooleanField(default=False)
+	slug = models.SlugField(max_length=100, default="")
+	features = models.ManyToManyField(Feature)
+	registration_number = models.CharField( max_length=30, blank=True )
+	vin = models.CharField( max_length=100, blank=True)
+	created_date = models.DateField(default=datetime.now, blank=True)
+	seller = models.ForeignKey(Seller,on_delete=models.CASCADE)
+      
+	class Meta:
+		ordering = ('title',)
+
+	def __str__(self):
+		return self.title
+
+class CarImage(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="car_images")
+    image = models.ImageField(upload_to="images/car_images/")
+
+    def __str__(self):
+        return f"Image for {self.car.make} {self.car.model}"
 
 
-# class CarImage(models.Model):
-#     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="car_images")
-#     image = models.ImageField(upload_to="images/car_images/")
-
-#     def __str__(self):
-#         return f"Image for {self.car.make} {self.car.model}"
 
 
 
-
-
-
-
-
-
-
-
-# @receiver(post_delete, sender=CarImage)
-# def delete_car_image_file(sender, instance, **kwargs):
-#     if instance.image:
-#         if os.path.isfile(instance.image.path):
-#             os.remove(instance.image.path)
+@receiver(post_delete, sender=CarImage)
+def delete_car_image_file(sender, instance, **kwargs):
+    if instance.image:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
             
-# @receiver(post_delete, sender=Car)
-# def delete_car_images_on_car_delete(sender, instance, **kwargs):
-#     for image in instance.images.all():
-#         if image.image and os.path.isfile(image.image.path):
-#             os.remove(image.image.path)
+@receiver(post_delete, sender=Car)
+def delete_car_images_on_car_delete(sender, instance, **kwargs):
+    for image in instance.images.all():
+        if image.image and os.path.isfile(image.image.path):
+            os.remove(image.image.path)
 
 
 
-#################house###################
+#################estate###################
 
 
-class House(models.Model):
+class Estate(models.Model):
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)    
     location = models.CharField(max_length=255)  
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -94,12 +238,12 @@ class House(models.Model):
         return self.title
 
 
-class HouseImage(models.Model):
-    house = models.ForeignKey(House, related_name='house_images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='image/house_images')
+class EstateImage(models.Model):
+    estate = models.ForeignKey(Estate, related_name='estate_images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='image/estate_images')
 
     def __str__(self):
-        return f"Image for {self.house.title}"
+        return f"Image for {self.estate.title}"
     
     
     
@@ -107,15 +251,15 @@ class HouseImage(models.Model):
 
     
     
-@receiver(post_delete, sender=HouseImage)
-def delete_house_image_file(sender, instance, **kwargs):
+@receiver(post_delete, sender=EstateImage)
+def delete_estate_image_file(sender, instance, **kwargs):
     if instance.image:
         if os.path.isfile(instance.image.path):
             os.remove(instance.image.path)
 
 
-@receiver(post_delete, sender=House)
-def delete_house_images_on_house_delete(sender, instance, **kwargs):
+@receiver(post_delete, sender=Estate)
+def delete_estate_images_on_estate_delete(sender, instance, **kwargs):
     for image in instance.images.all():
         if image.image and os.path.isfile(image.image.path):
             os.remove(image.image.path)
--- app/views.py
+++ app/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.hashers import check_password
 from django.views import View
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .models import Merchant
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import login
-class MerchantSignUpView(View):
+from .models import *
+
+
+###########seller###############
+class SellerSignUpView(View):
     template_name = 'signup.html'
     
     def get(self, request):
@@ -21,13 +24,13 @@ class MerchantSignUpView(View):
         password = request.POST.get('password')
         password1 = request.POST.get('password1')
 
-        if Merchant.objects.filter(email=email).exists():
+        if Seller.objects.filter(email=email).exists():
             messages.error(request, "البريد الإلكتروني مسجل بالفعل.")
             return render(request, self.template_name)
         if password != password1:
             messages.error(request, "كلمة المرور غير متساوية")
             return render(request, self.template_name)
-        Merchant.objects.create(
+        Seller.objects.create(
             first_name=first_name,
             last_name=last_name,
             phone=phone,
@@ -40,7 +43,7 @@ class MerchantSignUpView(View):
 
 
 
-class MerchantLoginView(View):
+class SellerLoginView(View):
     template_name = 'login.html'
 
     def get(self, request):
@@ -50,14 +53,234 @@ class MerchantLoginView(View):
         email = request.POST.get('email')
         password = request.POST.get('password')
         try:
-            merchant = Merchant.objects.get(email=email)
-            if check_password(password, merchant.password):
-                login(request, merchant) 
+            seller = get_object_or_404(Seller,email=email)
+            if check_password(password, seller.password):
+                login(request, seller) 
                 messages.success(request, "تم تسجيل الدخول بنجاح.")
                 return redirect('home') 
             else:
                 messages.error(request, "كلمة المرور غير صحيحة.")
-        except Merchant.DoesNotExist:
+        except Seller.DoesNotExist:
             messages.error(request, "المستخدم غير موجود.")
 
         return render(request, self.template_name)
+    
+    
+    
+        
+class sellerEditView(View):
+    template_name = 'edit_estate.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        return render(request, self.template_name)
+    
+    
+
+
+
+
+
+
+##########estate#############
+
+class EstateView(View):
+    template_name = 'estate.html'
+
+    def get(self, request,pk):
+        estate=get_object_or_404(Estate,pk=pk)
+        images=EstateImage.objects.get(estate=estate)
+        context={
+            'estate':estate,
+            'images':images
+        }
+        return render(request, self.template_name,context)
+
+
+
+
+
+class EstateEditView(View):
+    template_name = 'edit_estate.html'
+
+    def get(self, request, pk):
+        estate = get_object_or_404(Estate, pk=pk)
+        images = EstateImage.objects.filter(estate=estate) 
+        context = {
+            'estate': estate,
+            'images': images
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, pk):
+        estate = get_object_or_404(Estate, pk=pk)
+        images = EstateImage.objects.filter(estate=estate) 
+        data = request.POST
+        if "delete_image" in data:
+            image_id = data.get("delete_image")
+            image = get_object_or_404(EstateImage, id=image_id, estate=estate)
+            image.delete()
+            messages.success(request, "تم حذف الصورة بنجاح.")
+            return redirect('edit_estate', pk=pk) 
+        try:
+            estate.title = data.get("title")
+            estate.location = data.get("location")
+            estate.price = data.get("price")
+            estate.rooms = data.get("rooms")
+            estate.floor = data.get("floor")
+            estate.bathrooms = data.get("bathrooms")
+            estate.area = data.get("area")
+            estate.save()
+            messages.success(request, "تم تحديث بيانات المنزل بنجاح.")
+        except:
+            messages.error(request, "حدث خطأ أثناء حفظ البيانات.")
+
+        context = {
+            'estate': estate,
+            'images': images
+        }
+        return render(request, self.template_name, context)
+
+
+
+
+
+
+##############Car###############
+
+class CarView(View):
+    template_name = 'car.html'
+
+    def get(self, request,pk):
+        car = get_object_or_404(Car, pk=pk)
+        images = CarImage.objects.filter(car=car)
+        context = {
+            'car': car,
+            'images': images,
+            'selected_features': car.features.all(),  
+        }
+        return render(request, self.template_name, context)
+    
+    
+
+class CarAddView(View):
+    template_name = 'add_car.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request, pk):
+        data = request.POST
+        seller=request.user
+        features = request.POST.getlist('features') 
+        try:
+            car = Car.objects.create(
+                title=data.get("title"),
+                make=data.get("make"),
+                model=data.get("model"),
+                trim=data.get("trim"),
+                year=data.get("year"),
+                kilometers=data.get("kilometers"),
+                exterior_color=data.get("exterior_color"),
+                interior_color=data.get("interior_color"),
+                regional_spec=data.get("regional_spec"),
+                fuel_type=data.get("fuel_type"),
+                transmission=data.get("transmission"),
+                body_type=data.get("body_type"),
+                doors=data.get("doors"),
+                seating=data.get("seating"),
+                engine_capacity=data.get("engine_capacity"),
+                cylinders=data.get("cylinders"),
+                horsepower=data.get("horsepower"),
+                steering_side=data.get("steering_side"),
+                warranty=data.get("warranty"),
+                selling_price=data.get("selling_price"),
+                description=data.get("description"),
+                status=data.get("status"),
+                is_featured=bool(data.get("is_featured")),
+                registration_number=data.get("registration_number"),
+                vin=data.get("vin"),
+                seller=seller
+            )
+            car.features.set(features)
+            images = request.FILES.getlist('images')
+            for image in images:
+                CarImage.objects.create(car=car, image=image)
+            messages.success(request, "تم اضافة  السيارة بنجاح.")
+        except:
+            messages.error(request, "فشل  اضافة السيارة.")
+
+        return redirect('edit_car', pk=pk)
+    
+    
+    
+    
+class CarEditView(View):
+    template_name = 'edit_car.html'
+
+    def get(self, request, pk):
+        car = get_object_or_404(Car, pk=pk)
+        images = CarImage.objects.filter(car=car)
+        features = Feature.objects.all()  
+        context = {
+            'car': car,
+            'images': images,
+            'features': features,
+            'selected_features': car.features.all(),  
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, pk):
+        car = get_object_or_404(Car, pk=pk)
+        data = request.POST
+
+        if "delete_image" in data:
+            image_id = data.get("delete_image")
+            image = get_object_or_404(CarImage, id=image_id, car=car)
+            image.delete()
+            messages.success(request, "تم حذف الصورة بنجاح.")
+            return redirect('edit_car', pk=pk)
+
+        try:
+            car.title = data.get("title")
+            car.make = data.get("make")
+            car.model = data.get("model")
+            car.trim = data.get("trim")
+            car.year = data.get("year")
+            car.kilometers = data.get("kilometers")
+            car.exterior_color = data.get("exterior_color")
+            car.interior_color = data.get("interior_color")
+            car.regional_spec = data.get("regional_spec")
+            car.fuel_type = data.get("fuel_type")
+            car.transmission = data.get("transmission")
+            car.body_type = data.get("body_type")
+            car.doors = data.get("doors")
+            car.seating = data.get("seating")
+            car.engine_capacity = data.get("engine_capacity")
+            car.cylinders = data.get("cylinders")
+            car.horsepower = data.get("horsepower")
+            car.steering_side = data.get("steering_side")
+            car.warranty = data.get("warranty")
+            car.selling_price = data.get("selling_price")
+            car.description = data.get("description")
+            car.status = data.get("status")
+            car.is_featured = bool(data.get("is_featured"))
+            car.registration_number = data.get("registration_number")
+            car.vin = data.get("vin")
+            car.save()
+            
+            feature_ids = request.POST.getlist("features") 
+            car.features.set(feature_ids)  
+            
+            if request.FILES.getlist("images"):
+                for image in request.FILES.getlist("images"):
+                    CarImage.objects.create(car=car, image=image)
+
+            messages.success(request, "تم تحديث بيانات السيارة بنجاح.")
+        except :
+            messages.error(request, "فشل في تحديث بيانات السيارة.")
+        return redirect('edit_car', pk=pk)
+
+
